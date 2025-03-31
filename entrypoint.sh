0043dfdf6279a07aff4cb142a0b0138883f7077b
adrianty <adrian.ty@emerson.com>
2025-03-31 19:28:40 +0800

    removed entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Change ownership of /home/node/n8n and /home/node/.n8n to user "node" (1000:1000)
-chown -R 1000:1000 /home/node/n8n /home/node/.n8n
-chown -R node:node /home/node/n8n /home/node/.n8n
-
-# Execute the original command
-exec "$@"
